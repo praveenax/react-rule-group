91504fbd0e81ffa742e7c401c8cb724d710eda2f
praveen kumar a x <xav.axp@gmail.com>
2022-09-15 12:40:28 +0530

    Added Changes to implement basic Ui

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,48 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-rule-group'
+import { ReactRuleGroup } from 'react-rule-group'
 import 'react-rule-group/dist/index.css'
+const inputJson = [
+  {
+    "type": "group",
+    "rules": [
+      {
+        "type": "rule",
+        "text": "Select contacts in last 7 number"
+      },
+      {
+        "type": "group",
+        "rules": [
+          {
+            "type": "rule",
+            "text": "Select from emoji"
+          },
+          {
+            "type": "rule",
+            "text": "Contacts from Email Channel"
+          },
+          {
+            "type": "group",
+            "rules": [{
+              "type": "rule",
+              "text": "master_contact > gender equals F"
+            },
+            {
+              "type": "rule",
+              "text": "master_contact > birth_date is after current_date"
+            },],
+            "operator": "AND"
+          }
+        ],
+        "operator": "OR"
+      }
+    ],
+    "operator": "AND"
+  }
+]
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ReactRuleGroup text="Create React Library Example 😄" rules={inputJson} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const GroupView = ({ operator, rules, level }) => {
+  return (
+    <div className={styles.groupView}>
+      <div
+        className={styles.groupView2}
+        style={{ borderColor: `${operator === 'AND' ? 'blue' : 'orange'}` }}
+      ></div>
+      <div className={styles.groupView3}>
+        <div className={styles.operator}>{operator}</div>
+        {rules && rules.length > 0 && (
+          <ReactRuleGroup text='' rules={rules} level={level + 1} />
+        )}
+        <button className={styles.addFilterBtn}>Add Filter</button>
+        <button className={styles.addGroupBtn}>Add Group</button>
+      </div>
+    </div>
+  )
+}
+
+const RuleView = ({ data }) => {
+  return <div className={styles.ruleText}>{data}</div>
+}
+
+export const ReactRuleGroup = ({ text, rules, level = 0 }) => {
+  return (
+    <div className={styles.test} style={{ paddingLeft: `50px` }}>
+      {rules.map((r, i) => {
+        if (r.type === 'group') {
+          return (
+            <GroupView
+              key={i}
+              operator={r.operator}
+              rules={r.rules}
+              level={level}
+            />
+          )
+        }
+        if (r.type === 'rule') {
+          return <RuleView key={i} data={r.text} />
+        }
+      })}
+    </div>
+  )
 }
